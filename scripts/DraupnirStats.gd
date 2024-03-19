6de263086f9740114980cda33da1ba3410ec96fe
Zofia Szybalska <zochula111@gmail.com>
2024-03-19 15:19:47 +0100

    Add DruapnirStats Resource that hold infomations about Draupnirs

---
+++ scripts/DraupnirStats.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name DraupnirStats
+
+@export var name: String
+@export var cost: int = 1
+@export_multiline var description: String
+@export var icon: Texture2D
+@export var is_unlocked: bool = false

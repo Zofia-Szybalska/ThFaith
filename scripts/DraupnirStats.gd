b2e897e3d57cd618136fd3aaaa2228ad3a2a3fb0
Zofia Szybalska <zochula111@gmail.com>
2024-05-21 15:56:16 +0200

    Add init to draupnirStats resource

--- scripts/DraupnirStats.gd
+++ scripts/DraupnirStats.gd
@@ -7,5 +7,14 @@ class_name DraupnirStats
 @export var icon: Texture2D
 @export var locked_icon: Texture2D
 @export var is_unlocked: bool = false
+@export var effect: Script
 
 
+func _init(p_name = "Defult", p_cost = 1, p_description = "Defult description", p_icon = Texture2D.new(), p_locked_icon = Texture2D.new(), p_is_unlocked = false, p_effect = null):
+	name = p_name
+	cost = p_cost
+	description = p_description
+	icon = p_icon
+	locked_icon = p_locked_icon
+	is_unlocked = p_is_unlocked
+	effect = p_effect

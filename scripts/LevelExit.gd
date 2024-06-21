7c6fabdec7092c5dfe37eef7af34da0b38ca9686
Zofia Szybalska <zochula111@gmail.com>
2024-06-21 11:28:56 +0200

    Add background

--- scripts/LevelExit.gd
+++ scripts/LevelExit.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
-@export var next_scene: PackedScene = null
+@export var next_scene: String = ""
+@export var player_pos: Vector2 = Vector2.ZERO
 
 func _on_body_entered(body):
 	if body is Player:
-		get_tree().change_scene_to_packed(next_scene)
+		PlayerVariables.player_spawn_pos = player_pos
+		get_tree().change_scene_to_file(next_scene)
--- scripts/hud.gd
+++ scripts/hud.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var hp_label = %HPLabel
 @onready var currency_label = %CurrencyLabel
 @onready var currency_changed_lable = %CurrencyChangedLable
+@onready var dead_screen = $DeadScreen
 
 @export var time_showing_new_items: float = 5.0
 
@@ -14,6 +15,9 @@ func _ready():
 
 func update_health():
 	hp_label.text ="%s/%s" % [PlayerVariables.health, PlayerVariables.max_health]
+	if PlayerVariables.health == 0:
+		get_tree().paused = true
+		dead_screen.show()
 
 func update_currency(amount_changed: int = 0):
 	currency_label.text = str(PlayerVariables.currency)
@@ -25,3 +29,10 @@ func update_currency(amount_changed: int = 0):
 		currency_changed_lable.text += str(amount_changed)
 		var tween = get_tree().create_tween()
 		tween.tween_property(currency_changed_lable, "modulate", Color(1, 1, 1, 0), time_showing_new_items)
+
+
+func _on_button_pressed():
+	dead_screen.hide()
+	PlayerVariables.health = PlayerVariables.max_health
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/levels/midgard.tscn")

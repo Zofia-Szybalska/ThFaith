4671c55cab0933d998aeef9a791dec7e31f65a11
Zofia Szybalska <zochula111@gmail.com>
2025-01-05 01:28:25 +0100

    Add snow particels

---
+++ scenes/snow_particels.gd
@@ -0,0 +1,12 @@
+extends Node2D
+@onready var gpu_particles_2d = $GPUParticles2D
+
+
+func _process(delta):
+	var canvas = get_canvas_transform()
+	var top_left = -canvas.origin / canvas.get_scale()
+	var size = get_viewport_rect().size / canvas.get_scale()
+	
+	gpu_particles_2d.global_position.x = top_left.x + size.x/2
+	gpu_particles_2d.global_position.y = top_left.y - 50
+	gpu_particles_2d.process_material.set_emission_box_extents(Vector3(size.x, 50, 1))
